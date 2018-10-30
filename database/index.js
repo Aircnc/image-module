2570e16d7e3719d270312c4e9ec3a8afa13637fb
Luan Tran <luan.phuoc.tran.96@gmail.com>
2018-10-29 19:04:57 -0700

    Add circleCI config file

--- database/index.js
+++ database/index.js
@@ -3,11 +3,6 @@ const db = mongoose.connection;
 
 mongoose.connect('mongodb://127.0.0.1/images', { useNewUrlParser: true });
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-});
-
 const imageSchema = new mongoose.Schema({
   listingId: String,
   images: Array,
